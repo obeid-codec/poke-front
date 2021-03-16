d8591cc5bfc415076b8dd9d42e783ba943dda5f4
Reza <hajipour.mrh@gmail.com>
2021-03-16 18:14:25 +0100

    pokemoninfo

--- src/components/pokemonInfo/PokemonInfo.js
+++ src/components/pokemonInfo/PokemonInfo.js
@@ -1,20 +1,54 @@
-import React,{useState,useEffect} from 'react'
-import {useParams} from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { useParams } from "react-router-dom";
 import Axios from "axios";
-
+// import getApiData from '../utils/Api';
 const PokemonInfo = () => {
-    const {id, info} = useParams();
-    const [pokemonInfo, setPokemonInfo]= useState();
+    const { id, info } = useParams();
+    const [pokemonInfo, setPokemonInfo] = useState();
 
-    useEffect(()=>{
-        const url=`https://wbspokemon.herokuapp.com/pokemon/${id}/${info}`
 
+    let getApiData = (url) => {
         Axios.get(url)
-        
+    }
+
+
+    useEffect(() => {
+        const url = `https://wbspokemon.herokuapp.com/pokemon/${id}/${info}`
+        if (info === 'name') {
+            getApiData(`https://wbspokemon.herokuapp.com/pokemon/${id}/name`).then((res) => {
+                setPokemonInfo(res)
+                    .catch((err) => {
+                        console.error(err)
+                    })
+            })
+        } else if (info === 'type') {
+            getApiData(`https://wbspokemon.herokuapp.com/pokemon/${id}/type`).then((res) => {
+                setPokemonInfo(res)
+                    .catch((err) => {
+                        console.error(err)
+                    })
+            })
+        } else if (info === 'base') {
+            getApiData(`https://wbspokemon.herokuapp.com/pokemon/${id}/base`).then((res) => {
+                setPokemonInfo(res)
+                    .catch((err) => {
+                        console.error(err)
+                    })
+            })
+        } else {
+            getApiData(`https://wbspokemon.herokuapp.com/pokemon/${id}`).then((res) => {
+                setPokemonInfo(res)
+                    .catch((err) => {
+                        console.error(err)
+                    })
+            })
+        }
+
+
     })
     return (
         <div>
-            PokemonInfo
+            <pre>{JSON.stringify(pokemonInfo)}</pre>
         </div>
     )
 }
---
+++ src/components/utils/Api.js
@@ -0,0 +1,9 @@
+const getApiData = (url) => {
+    try {
+        return fetch(url).then(response => response.json())
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default { getApiData };
